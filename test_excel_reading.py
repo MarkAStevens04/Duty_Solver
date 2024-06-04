123bafefd2b6abb2fcd7ecaf3419bed1ab54cbe2
Mark <doylethecat@gmail.com>
2024-06-04 19:33:01 -0400

    [CODE] Excel Test
    Added some preliminary tests for excel

---
+++ test_excel_reading.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+# Define variable to load the dataframe
+dataframe = openpyxl.load_workbook("Excel_Files/Test.xlsx")
+
+# Define variable to read sheet
+dataframe1 = dataframe.active
+
+# Iterate the loop to read the cell values
+for row in range(1, dataframe1.max_row):
+    for col in dataframe1.iter_cols(1, dataframe1.max_column):
+        print(col[row].value)
+    print(f'-----')

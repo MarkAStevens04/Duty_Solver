364237f5cc4bf51e83582a89a38452b83e020cc5
Mark <doylethecat@gmail.com>
2024-06-03 23:22:28 -0400

    [CODE] Slight optimizations
    sorted based on availability. Figure that it's better to solve the ones with less availability first, then solve the more difficult ones

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 names = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8']
 num_booked = [0, 0, 0, 0, 0, 0, 0, 0]
-max_diff = 1
+max_diff = 0.5
 num_found = [0]
 
 
@@ -27,14 +27,25 @@ availability = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
                 [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]]
 
-availability =[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+# this is the solution!
+# availability =[[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+#                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+#                [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+#                [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+#                [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+#                [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+#                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+# this one is extra hard!
+availability =[[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+               [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+               [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 # availability = [[1, 1, 1],
 #                 [1, 1, 1],
@@ -66,16 +77,15 @@ def recursive_solver(availability, index, names, final_availability):
         # return True
         # num_found[0] = num_found[0] + 1
         # print(num_found[0])
-        # return False
+        return False
 
 
-    for p1_i in range(len(names)):
-        for p2_i in range(p1_i + 1, len(names)):
+    for p1_i in range(len(availability)):
+        for p2_i in range(p1_i + 1, len(availability)):
             # quickly terminates bad runs!
             if num_booked[p1_i] > avg + max_diff or num_booked[p2_i] > avg + max_diff:
                 return False
-
-
+            # print(index)
             # iterates through every possible combination of names
             p1_availability = availability[p1_i][index]
             p2_availability = availability[p2_i][index]
@@ -112,6 +122,49 @@ def find_final_availability(availability):
     return final
 
 
+def optimize_order(availability):
+    # orders people based on their availability
+    #
+    # returns the new availability, alongside a dict_mapping.
+    # the dict mapping shows for each index produced, what original index it came from.
+    # for example {0:6} means that the first row of the new array came from the 6th row of the original array
+    num_days_free = dict()
+    dict_mapping = dict()
+    for i in range(len(availability)):
+        num_days_free[i] = 0
+        dict_mapping[i] = 0
+    num_f = [0] * len(availability)
+    for row_i in range(len(availability)):
+        for day in availability[row_i]:
+            if day == 1:
+                num_f[row_i] = num_f[row_i] + 1
+                num_days_free[row_i] = num_days_free[row_i] + 1
+
+    tuples = []
+    for input in range(len(num_f)):
+        tuples.append((num_f[input], input))
+    tuples.sort()
+    num_f.sort()
+    # print(tuples)
+    # print(dict_mapping)
+
+    for found_i in range(len(tuples)):
+        dict_mapping[tuples[found_i][1]] = found_i
+
+    # print(dict_mapping)
+
+    sorted_array = []
+    for i in range(len(tuples)):
+        sorted_array.append(availability[tuples[i][1]])
+
+    # for row in sorted_array:
+    #     print(row)
+    return sorted_array, dict_mapping
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -127,12 +180,15 @@ if __name__ == "__main__":
     #                       [0, 0, 0],
     #                       [0, 0, 0]]
 
-    print(f'final availability:')
+    # print(f'final availability:')
     final_availability = find_final_availability(availability)
-    for row in final_availability:
-        print(row)
-    print()
-    print(f"---")
+    # for row in final_availability:
+    #     print(row)
+    # print()
+    # print(f"---")
+    print(f'---')
+    availability, dict_mapping = optimize_order(availability)
+    print(f'---')
 
     if not recursive_solver(availability, 0, names, final_availability):
         print("----- no valid combo found! -----")
